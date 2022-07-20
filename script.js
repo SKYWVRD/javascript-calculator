0f87b74bc2eb62e4ff97ed05f4e85d7688a59e2c
Sean Boonzaier <boonzaiersa@gmail.com>
2022-07-20 21:39:55 +0200

    Add button query links

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const displayScreen = document.querySelector('.display-screen')
 
+const zeroButton = document.querySelector('#zero');
+const oneButton = document.querySelector('#one');
+const twoButton = document.querySelector('#two');
+const threeButton = document.querySelector('#three');
+const fourButton = document.querySelector('#four');
+const fiveButton = document.querySelector('#five');
+const sixButton = document.querySelector('#six');
+const sevenButton = document.querySelector('#seven');
+const eightButton = document.querySelector('#eight');
+const nineButton = document.querySelector('#nine');
+const equalsButton = document.querySelector('#equals');
+const addButton = document.querySelector('#add');
+const subtractButton = document.querySelector('#subtact');
+const multiplyButton = document.querySelector('#multiply');
+const divideButton = document.querySelector('#divide');
+
+
 let displayValue = "";
 
 function updateDisplayValue(text){
@@ -9,4 +26,6 @@ function updateDisplayValue(text){
 
 updateDisplayValue(displayValue);
 
-console.log(displayValue)
+zeroButton.addEventListener('click', ()=>{
+    updateDisplayValue('0');
+})
